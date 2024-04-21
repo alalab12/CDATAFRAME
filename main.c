af7985851f41d2cab2c6fbcd0053a1bedfc24b46
alalab12 <101478194+alalab12@users.noreply.github.com>
2024-04-21 11:27:10 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,26 +1,65 @@
-#include "fonctions.h"
-#include "fonctionnalites.h"
+#include "column.h"
+#include "cdataframe.h"
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
 
 int main(){
-    COLUMN *mycol = create_column("colonne 1");
-    insert_value(mycol, 1);
-    insert_value(mycol, 2);
-    insert_value(mycol, 3);
+    //COLUMN *Column_1 = create_column("colonne 1");
 
-    print_column(mycol);
-
-    int x = 3;
-    printf("Le nombre d'occurence de %d: %d\n", x, nb_occurence(mycol, x));
-
-    free_column(mycol);
+    //insert_value(Column_1, 1);
+    //insert_value(Column_1, 2);
+    //insert_value(Column_1, 3);
+    //insert_value(Column_1,4);
 
 
+    //print_column(Column_1);
 
 
+    //int value = 2;
+    //int occurrences = nb_occurence(Column_1, value);
+    //printf("La valeur %d est presente %d dans la colonne\n", value, occurrences);
+
+
+    //int position = x_pos(Column_1, value);
+    //printf("La position de valeur %d est %d\n", value, position);
+
+    //int superieur = val_sup(Column_1, value);
+    //printf("Il y a %d valeurs superieur a %d\n", superieur, value);
+
+
+    //int inferieur = val_inf(Column_1, value);
+    //printf("Il y a %d valeurs inferieur a %d\n", inferieur, value);
+
+
+    //int egal = val_egal(Column_1, value);
+    //printf("Il y a %d valeurs egal a %d\n", egal, value);
+
+    //free_column(&Column_1);
+
+
+    //CDATAFRAME dataframe;
+
+    //remplissage_utilisateur(&dataframe);
+
+    //afficher_dataframe(&dataframe);
+
+    // Scenario 2: Fill the DataFrame with pre-defined data
+    //int rows = 3;
+    //int columns = 4;
+    //int** data = (int**)malloc(rows * sizeof(int*));
+
+    }
+    //remplissage_dur(&dataframe, data, rows, columns);
+
+
+    //afficher_dataframe(&dataframe);
+
+
+    //afficher_certaines_colonne(&dataframe, 2);
+
+    //free(dataframe.data);
 
     return 0;
-}
 
+}
