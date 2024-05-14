82f295936800a493ebc11f4c3ee8b060ad0607d0
alalab12 <101478194+alalab12@users.noreply.github.com>
2024-05-14 17:02:15 +0200

    Update cdatadrame.h

--- cdatadrame.h
+++ cdatadrame.h
@@ -1,9 +1,7 @@
-//
-// Created by audel on 15/04/2024.
-//
 
-#ifndef CDATAFRAME_FONCTIONNALITES_H
-#define CDATAFRAME_FONCTIONNALITES_H
+
+#ifndef CDATAFRAME_CDATAFRAME_H
+#define CDATAFRAME_CDATAFRAME_H
 
 #include "column.h"
 
@@ -15,42 +13,42 @@ typedef struct {
 
 }CDATAFRAME;
 
-enum enum_type
- {
- NULLVAL = 1 , UINT, INT, CHAR, FLOAT, DOUBLE, STRING, STRUCTURE
- };
- typedef enum enum_type ENUM_TYPE;
-
-union column_type{
- unsigned int uint_value;
- signed int int_value;
- char char_value;
- float float_value;
- double double_value;
- char* string_value;
- void* struct_value;
- };
- typedef union column_type COL_TYPE ;
 
 CDATAFRAME* creation_dataframe_vide();
 
 void remplissage_utilisateur(CDATAFRAME* dataframe);
+
 void remplissage_dur(CDATAFRAME* dataframe, int** data, int number_rows, int number_columns);
+
 void afficher_dataframe(CDATAFRAME* dataframe);
+
 void afficher_certaines_colonne(CDATAFRAME* dataframe,int limite);
+
 void afficher_certaines_ligne(CDATAFRAME* dataframe,int limite);
+
 void ajouter_ligne_valeur(CDATAFRAME * dataframe, int* val);
+
 void ajouter_colonne(CDATAFRAME * dataframe, COLUMN* column);
+
 void supprimer_colonne(CDATAFRAME * dataframe, int index);
+
 void renomer_colonne(CDATAFRAME * dataframe, int index, char* nouveau_titre);
+
 int acceder_valeur(CDATAFRAME * dataframe, int ligne, int colonne);
+
 void remplacer_valeur(CDATAFRAME * dataframe, int row, int col, int value);
+
 void afficher_titre_colonne(CDATAFRAME* dataframe);
+
 int nb_lignes(CDATAFRAME* dataframe);
+
 int nb_colonnes(CDATAFRAME* dataframe);
+
 int nb_valeur_egale(CDATAFRAME* dataframe, int val);
+
 int nb_valeur_sup(CDATAFRAME* dataframe, int val);
+
 int nb_valeur_inf(CDATAFRAME* dataframe, int val);
 
 
-#endif //CDATAFRAME_FONCTIONNALITES_H
+#endif //CDATAFRAME_CDATAFRAME_H
