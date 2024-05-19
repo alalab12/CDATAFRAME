c544c6fcc7443829eae7115d75ee00bd1d350e3a
alalab12 <101478194+alalab12@users.noreply.github.com>
2024-05-19 15:40:45 +0200

    Update cdatadrame.h

--- cdatadrame.h
+++ cdatadrame.h
@@ -21,24 +21,24 @@ void dataframe_user_input(CDataframe* cdf);
 void dataframe_hardcoded(CDataframe* cdf);
 
 // Display
-void print_dataframe(const CDataframe* cdf);
-void print_dataframe_rows(const CDataframe* cdf, int row_limit);
-void print_dataframe_columns(const CDataframe* cdf, int col_limit);
+void print_dataframe(CDataframe* cdf);
+void print_dataframe_rows(CDataframe* cdf, int row_limit);
+void print_dataframe_columns( CDataframe* cdf, int col_limit);
 
 
 int add_row(CDataframe* cdf, int* values);
 int delete_row(CDataframe* cdf, int row_index);
-int add_column(CDataframe* cdf, const char* title);
+int add_column(CDataframe* cdf, char* title);
 int delete_column(CDataframe* cdf, int col_index);
-int rename_column(CDataframe* cdf, int col_index, const char* new_title);
-int value_exists(const CDataframe* cdf, int value);
+int rename_column(CDataframe* cdf, int col_index, char* new_title);
+int value_exists(CDataframe* cdf, int value);
 int access_value(CDataframe* cdf, int row, int col, int* value);
 int replace_value(CDataframe* cdf, int row, int col, int value);
-void print_column_titles(const CDataframe* cdf);
+void print_column_titles( CDataframe* cdf);
 
 
-int num_rows(const CDataframe* cdf);
-int num_columns(const CDataframe* cdf);
-int count_equal(const CDataframe* cdf, int x);
-int count_greater(const CDataframe* cdf, int x);
-int count_less(const CDataframe* cdf, int x);
+int num_rows(CDataframe* cdf);
+int num_columns(CDataframe* cdf);
+int count_equal(CDataframe* cdf, int x);
+int count_greater(CDataframe* cdf, int x);
+int count_less(CDataframe* cdf, int x);
