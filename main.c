225d959333cf75bcc30a68b970b0d3e261638a8d
alalab12 <101478194+alalab12@users.noreply.github.com>
2024-05-19 15:39:13 +0200

    Update main.c

--- main.c
+++ main.c
@@ -5,59 +5,20 @@
 #include <stdio.h>
 
 int main(){
-    //COLUMN *Column_1 = create_column("colonne 1");
+  CDataframe* df = create_dataframe();
+    
+    add_column(df, "Column1");
+    add_column(df, "Column2");
 
-    //insert_value(Column_1, 1);
-    //insert_value(Column_1, 2);
-    //insert_value(Column_1, 3);
-    //insert_value(Column_1,4);
-
-
-    //print_col(Column_1);
-
-
-    //int value = 2;
-    //int occurrences = nb_occurence(Column_1, value);
-    //printf("La valeur %d est presente %d dans la colonne\n", value, occurrences);
-
-
-    //int position = x_pos(Column_1, value);
-    //printf("La position de valeur %d est %d\n.", value, position);
-
-    //int superieur = val_sup(Column_1, value);
-    //printf("Il y a %d valeurs superieur à %d\n.", superieur, value);
-
-
-    //int inferieur = val_inf(Column_1, value);
-    //printf("Il y a %d valeurs inferieur à %d\n.", inferieur, value);
-
-
-    //int egal = val_egal(Column_1, value);
-    //printf("Il y a %d valeurs egal à %d\n.", egal, value);
-
-    //delete_column(&Column_1);
-
-
-    //CDATAFRAME dataframe;
-
-    //remplissage_utilisateur(&dataframe);
-
-    //afficher_dataframe(&dataframe);
-
-    // Scenario 2: Fill the DataFrame with pre-defined data
-    //int rows = 3;
-    //int columns = 4;
-    //int** data = (int**)malloc(rows * sizeof(int*));
-
-    //remplissage_dur(&dataframe, data, rows, columns);
-
-
-    //afficher_dataframe(&dataframe);
-
-
-    //afficher_certaines_colonne(&dataframe, 2);
-
-    //free(dataframe.data);
+    
+    int values1[] = {1, 2};
+    int values2[] = {3, 4};
+    add_row(df, values1);
+    add_row(df, values2);
+    // Display dataframe
+    print_dataframe(df);
+    
+    free_dataframe(df);
 
     return 0;
 
