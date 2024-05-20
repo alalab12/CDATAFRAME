bdb94003e9eb997bae79631353338d4cf0aa062a
JulieBr14 <152064227+JulieBr14@users.noreply.github.com>
2024-05-20 16:42:57 +0200

    Update cdatadrame.h

--- cdatadrame.h
+++ cdatadrame.h
@@ -19,13 +19,13 @@ CDataframe* creation_dataframe();
 void liberer_dataframe(CDataframe* cdf);
 
 //Filling
-void dataframe_remplissage_utisateur(CDataframe* cdf);
-void dataframe_dur(CDataframe* cdf);
+void dataframe_remplissage_utilisateur(CDataframe* cdf);
+void dataframe_remplissage_dur(CDataframe* cdf);
 
 // Display
 void afficher_dataframe(CDataframe* cdf);
 void afficher_dataframe_lignes(CDataframe* cdf, int row_limit);
-void print_dataframe_colonnes( CDataframe* cdf, int col_limit);
+void afficher_dataframe_colonnes( CDataframe* cdf, int col_limit);
 
 //Handling
 int ajouter_ligne(CDataframe* cdf, int* values);
@@ -33,9 +33,9 @@ int supprimer_ligne(CDataframe* cdf, int row_index);
 int ajouter_colonne(CDataframe* cdf, char* title);
 int suprimer_colonne(CDataframe* cdf, int col_index);
 int renomer_colonne(CDataframe* cdf, int col_index, char* new_title);
-int value_exists(CDataframe* cdf, int value);
+int valeur_exists(CDataframe* cdf, int value);
 int acceder_valeur(CDataframe* cdf, int row, int col, int* value);
-int remplacer_value(CDataframe* cdf, int row, int col, int value);
+int remplacer_valeur(CDataframe* cdf, int row, int col, int value);
 void afficher_titres( CDataframe* cdf);
 
 //Functionality
