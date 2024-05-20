b8c6742a2a19de9264882343f3e26e0a929947f1
JulieBr14 <152064227+JulieBr14@users.noreply.github.com>
2024-05-20 18:24:22 +0200

    Update cdatadrame.h

--- cdatadrame.h
+++ cdatadrame.h
@@ -15,30 +15,30 @@ typedef struct {
 //Creation
 CDataframe* creation_dataframe();
 
-//Removal
+//suppression
 void liberer_dataframe(CDataframe* cdf);
 
-//Filling
+//Remplissage
 void dataframe_remplissage_utilisateur(CDataframe* cdf);
 void dataframe_remplissage_dur(CDataframe* cdf);
 
-// Display
+// Affichage
 void afficher_dataframe(CDataframe* cdf);
 void afficher_dataframe_lignes(CDataframe* cdf, int row_limit);
 void afficher_dataframe_colonnes( CDataframe* cdf, int col_limit);
 
-//Handling
+//Operation
 int ajouter_ligne(CDataframe* cdf, int* values);
 int supprimer_ligne(CDataframe* cdf, int row_index);
 int ajouter_colonne(CDataframe* cdf, char* title);
-int suprimer_colonne(CDataframe* cdf, int col_index);
+int supprimer_colonne(CDataframe* cdf, int col_index);
 int renomer_colonne(CDataframe* cdf, int col_index, char* new_title);
 int valeur_exists(CDataframe* cdf, int value);
 int acceder_valeur(CDataframe* cdf, int row, int col, int* value);
 int remplacer_valeur(CDataframe* cdf, int row, int col, int value);
 void afficher_titres( CDataframe* cdf);
 
-//Functionality
+//Fonctionnalité
 int nombre_ligne(CDataframe* cdf);
 int nombre_colonne(CDataframe* cdf);
 int count_egal(CDataframe* cdf, int x);
